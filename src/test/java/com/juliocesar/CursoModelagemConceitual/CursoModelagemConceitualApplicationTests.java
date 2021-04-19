e02ffcff43ef5b709c53dfc7019e76d9f7793df4
Julio Cesar <julio_m3_@hotmail.com>
2021-04-19 20:38:12 -0300

    Criação de Projeto

---
+++ src/test/java/com/juliocesar/CursoModelagemConceitual/CursoModelagemConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliocesar.CursoModelagemConceitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoModelagemConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
