9c7b4cd08bf500d131a3dbccd9e61a9d90cf4b20
Julio Cesar <julio_m3_@hotmail.com>
2021-04-20 18:18:57 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/juliocesar/CursoModelagemConceitual/domain/Categoria.java
+++ src/main/java/com/juliocesar/CursoModelagemConceitual/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.juliocesar.CursoModelagemConceitual.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -16,6 +19,9 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id; 
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List <Produto> produtos = new ArrayList<>(); 
 	
 	public Categoria() {	
 	}
@@ -42,6 +48,14 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
+	public List <Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List <Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -66,6 +80,6 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 	
 }
