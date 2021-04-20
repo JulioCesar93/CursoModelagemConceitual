60172e2be8b2ef19c8017aa9fbff78e38c99d9cd
Julio Cesar <julio_m3_@hotmail.com>
2021-04-19 21:04:01 -0300

    Testando o Rest

--- src/main/java/com/juliocesar/CursoModelagemConceitual/CursoModelagemConceitualApplication.java
+++ src/main/java/com/juliocesar/CursoModelagemConceitual/CursoModelagemConceitualApplication.java
@@ -8,6 +8,9 @@ public class CursoModelagemConceitualApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoModelagemConceitualApplication.class, args);
+		
+		
+		
 	}
 
 }
